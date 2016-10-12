b88570fcb10d26f82384e9e40c3d5bb7f6e1f7be
yuichi.watanabe <yuichi.watanabe@booklive.co.jp>
2016-10-12 11:52:51 +0900

    Initial Commit

---
+++ Operation_practice_06/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Operation_practice_06
+//
+//  Created by yuichi.watanabe on 2016/10/12.
+//  Copyright © 2016年 yuichi.watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
