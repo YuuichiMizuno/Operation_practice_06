6b92091dc69144abb3be13a5a8305bf3ef66ff02
yuichi.watanabe <yuichi.watanabe@booklive.co.jp>
2016-10-12 16:23:25 +0900

    update

---
+++ Operation_practice_06/TestOperationServiceA.swift
@@ -0,0 +1,86 @@
+//
+//  TestOperationsService.swift
+//  Operation_practice_06
+//
+//  Created by yuichi.watanabe on 2016/10/12.
+//  Copyright © 2016年 yuichi.watanabe. All rights reserved.
+//
+/* queueを使わず、複数のOperationを実行させようとすると複雑になった
+ * Operation一つでどこまで可能か検証する
+ * 結論としては、addするのは１回に限定して、その中で条件に応じてprivateを呼び分ければ可能
+ * GCDのDispatchQueue.mainは使わない
+ */
+
+import Foundation
+
+
+class TestOperationServiceA : BlockOperation
+{
+    let sampleId : String
+    
+    init( sampleId s : String )
+    {
+        self.sampleId = s
+        super.init()
+
+
+        //        // 1
+        //        self.addExecutionBlock {
+        //            DispatchQueue.main.async {
+        //                self.testOperation(operationNumber: 1, timeInterval: 1.5)
+        //            }
+        //        }
+        //
+        //        // 2
+        //        self.addExecutionBlock {
+        //            DispatchQueue.main.async {
+        //                self.testOperation(operationNumber: 2, timeInterval: 0.5)
+        //            }
+        //        }
+        //
+        //        // 3
+        //        self.addExecutionBlock {
+        //            DispatchQueue.main.async {
+        //                self.testOperation(operationNumber: 3, timeInterval: 0.3)
+        //            }
+        //        }
+        
+        
+        // 1 2 3
+        self.addExecutionBlock {
+            self.testOperation(operationNumber: 1, timeInterval: 1.5)
+            self.testOperation(operationNumber: 2, timeInterval: 0.5)
+            self.testOperation(operationNumber: 3, timeInterval: 0.3)
+        }
+        
+        
+    }
+    
+// operation内容だけを書いて実行したら、順番がバラバラになった
+//Operation3::  <NSThread: 0x618000079d80>{number = 3, name = (null)}
+//Operation2::  <NSThread: 0x60800007a980>{number = 4, name = (null)}
+//Operation1::  <NSThread: 0x608000078bc0>{number = 1, name = main}
+    
+// 一つのblockにまとめて書いてみた、やはり順番は守られた((成功)メインスレッドに統一された)
+//Operation1:: <NSThread: 0x608000073340>{number = 1, name = main}
+//Operation3:: <NSThread: 0x608000073340>{number = 1, name = main}
+//Operation2:: <NSThread: 0x608000073340>{number = 1, name = main}
+    
+//self.waitUntilFinished() はシングルOperation内なので、使えない
+    
+//DispatchQueue.main.async を使用した結果、順番はバラバラだった(メインスレッドに統一されたが、)
+// ただ問題がある、スレッドを意識しないコンセプトが意味なくなっている
+// operationの皮を被ったGCD
+//Operation1:: <NSThread: 0x608000263fc0>{number = 1, name = main}
+//Operation2:: <NSThread: 0x608000263fc0>{number = 1, name = main}
+//Operation3:: <NSThread: 0x608000263fc0>{number = 1, name = main}
+    
+    
+    fileprivate func testOperation(operationNumber o: Int, timeInterval t: TimeInterval)
+    {
+        print( "Operation\(o):: \(Thread.current)" )
+        Thread.sleep( forTimeInterval: t )
+    }
+    
+    
+}
--- Operation_practice_06/ViewController.swift
+++ Operation_practice_06/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    override func viewDidAppear(_ animated: Bool)
+    {
+        let operation = TestOperationServiceA(sampleId: "A014536924")
+            operation.start()
+    }
 
 }
 
